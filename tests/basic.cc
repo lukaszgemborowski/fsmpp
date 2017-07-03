759e7dc57d1f976b6f4bafc0e3fe81a724633f0b
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-07-03 19:07:48 +0200

    tests: use-case demo in test
    basically test does not check anything, just compilation and immunity to
    segfault. :-)

--- tests/basic.cc
+++ tests/basic.cc
@@ -1,3 +1,59 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include <fsm.hpp>
+#include <iostream>
+
+struct PowerOff {};
+struct PowerOn {};
+struct Reset {};
+struct StartOs {};
+
+struct State_Off
+{
+	void enter() { std::cout << "State_Off enter\n"; }
+	void exit() { std::cout << "State_Off exit\n"; }
+
+	void event(const PowerOn &) { std::cout << "PowerOn event\n"; }
+};
+
+struct State_Booting
+{
+	void enter() { std::cout << "State_Booting enter\n"; }
+	void exit() { std::cout << "State_Booting exit\n"; }
+
+	void event(const StartOs &) { std::cout << "StartOs event\n"; }
+	void event(const PowerOff &) { std::cout << "PowerOff event\n"; }
+};
+
+struct State_RunningOs
+{
+	void enter() { std::cout << "State_RunningOs enter\n"; }
+	void exit() { std::cout << "State_RunningOs exit\n"; }
+
+	void event(const PowerOff &) { std::cout << "PowerOff event\n"; }
+	void event(const Reset &) { std::cout << "Reset event\n"; }
+};
+
+using transitions = meta::list<
+	// normal startup off -> booting -> os
+	fsm::transition_definition<State_Off    , PowerOn, State_Booting>,
+	fsm::transition_definition<State_Booting, StartOs, State_RunningOs>,
+
+	// switching off transitions
+	fsm::transition_definition<State_Booting, PowerOff, State_Off>,
+	fsm::transition_definition<State_RunningOs, PowerOff, State_Off>,
+
+	// can be reset only in OS
+	fsm::transition_definition<State_RunningOs, Reset, State_Booting>
+>;
+
+fsm::fsm<transitions> computer;
+
+TEST_CASE("basic transitions", "[fsm]")
+{
+	computer.on(PowerOn{});
+	computer.on(StartOs{});
+	computer.on(Reset{});
+	computer.on(StartOs{});
+	computer.on(PowerOff{});
+}
