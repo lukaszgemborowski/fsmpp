945110e8d8e7eca137701be0fd062c2aa733c822
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-07-03 19:56:57 +0200

    fsm: code cleanup and some code comments

--- include/fsm.hpp
+++ include/fsm.hpp
@@ -30,17 +30,6 @@ struct state {
 	}
 };
 
-} // namespace detail
-
-template<typename S1, typename E, typename S2>
-struct transition
-{
-	using start_t = detail::state<S1>;
-	using stop_t = detail::state<S2>;
-	using event_t = E;
-};
-
-
 struct transition_to_state {
 	template<typename T>
 	using invoke = typename T::start_t;
@@ -52,60 +41,91 @@ using all_states = meta::transform<T, transition_to_state>;
 template<typename T>
 using all_unique_states = meta::unique<all_states<T>>;
 
+} // namespace detail
+
+// defines one transition between two states, this is type triple:
+// StartState, Event trigger, StopState
+template<typename S1, typename E, typename S2>
+struct transition
+{
+	using start_t = detail::state<S1>;
+	using stop_t = detail::state<S2>;
+	using event_t = E;
+};
+
 template<typename Transitions>
 struct fsm
 {
+private:
+	// helper templates all down to the public section
+
+	// determine tuple type based on provided transitios
+	using states_tuple_t = meta::apply<meta::quote<std::tuple>, detail::all_unique_states<Transitions>>;
+	using state_count = std::tuple_size<states_tuple_t>;
+
 	struct state_events {
 		template<typename T>
 		using invoke = meta::list<typename T::start_t, typename T::event_t>;
 	};
 
+	// create list of lists. This is StartState, Event extract from transitions
 	using all_state_events = meta::transform<Transitions, state_events>;
-	using states_t = meta::apply<meta::quote<std::tuple>, all_unique_states<Transitions>>;
 
+	// count all (S[tartState], E[vent]) pairs in provided list
+	// this one is needed to determine if specified state S is handling event E
 	template<typename S, typename E>
 	using handle_event = meta::count<
 		all_state_events,
 		meta::list<S, E>>;
 
+	// meta-function for checking if provided T (struct transition) has the same S[tate] and E[vent]
+	// as provided in template arguments
 	template<typename S, typename E>
 	struct check_dest {
 		template<typename T>
 		using invoke = meta::and_<std::is_same<S, typename T::start_t>, std::is_same<E, typename T::event_t>>;
 	};
-	
+
+	// selecting transition matching StartState-Event criteria
 	template<typename S, typename E>
 	using destination = meta::front<meta::find_if<Transitions, check_dest<S, E>>>;
 
+	// determining stop state from StartState, Event pair
 	template<typename S, typename E>
 	using destination_state = typename destination<S, E>::stop_t;
 
 	template<typename I>
-	using current_state = std::tuple_element_t<I::value, states_t>;
-
-	template<typename T>
-	using state_to_index = meta::find_index<meta::as_list<states_t>, T>;
+	using index_to_state = std::tuple_element_t<I::value, states_tuple_t>;
 
 	template<typename I, typename E>
-	using next_state = destination_state<current_state<I>, E>;
+	using next_state = destination_state<index_to_state<I>, E>;
+
+	template<typename T>
+	using state_to_index = meta::find_index<meta::as_list<states_tuple_t>, T>;
 
 	template<typename I, typename E>
 	using next_state_index = state_to_index<next_state<I, E>>;
 
-	fsm()
+	template<typename I>
+	using state_t_by_index = std::tuple_element_t<I::value, states_tuple_t>;
+
+public:
+	fsm() :
+		current (0)
 	{
-		current = 0;
 		std::get<0>(states).event(detail::EvEnterState{});
 	}
 
+	// handle event E
 	template<typename E>
 	void on(const E &event)
 	{
-		onCheck(event, current, meta::make_index_sequence<state_count::value>{});
+		onForAllImpl(event, current, meta::make_index_sequence<state_count::value>{});
 	}
 
+private:
 	template<typename E, std::size_t... Is>
-	void onCheck(const E &event, std::size_t atIdx, meta::index_sequence<Is...>)
+	void onForAllImpl(const E &event, std::size_t atIdx, meta::index_sequence<Is...>)
 	{
 		int dummy[state_count::value] = {
 			(
@@ -116,8 +136,8 @@ struct fsm
 	}
 
 	template<typename E, typename I>
-	std::enable_if_t<handle_event<std::tuple_element_t<I::value, states_t>, E>::value == 1>
-	onImpl(const E &event, std::size_t atIdx, std::tuple_element_t<I::value, states_t>* = nullptr)
+	std::enable_if_t<handle_event<state_t_by_index<I>, E>::value == 1>
+	onImpl(const E &event, std::size_t atIdx, std::tuple_element_t<I::value, states_tuple_t>* = nullptr)
 	{
 		if (I::value == atIdx) {
 			std::get<I::value>(states).event(event);
@@ -132,14 +152,13 @@ struct fsm
 	}
 
 	template<typename E, typename I>
-	std::enable_if_t<handle_event<std::tuple_element_t<I::value, states_t>, E>::value == 0>
-	onImpl(const E &event, std::size_t atIdx, std::tuple_element_t<I::value, states_t>* = nullptr)
+	std::enable_if_t<handle_event<state_t_by_index<I>, E>::value == 0>
+	onImpl(const E &event, std::size_t atIdx, std::tuple_element_t<I::value, states_tuple_t>* = nullptr)
 	{
 	}
 
 private:
-	states_t states;
-	using state_count = std::tuple_size<states_t>;
+	states_tuple_t states;
 	int current;
 };
 
