ca06235b6a5f263f9391d883125a4773cbacd9ac
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-08-08 19:45:03 +0200

    tests: create simplest possible test case
    mainly for demonstrational purposes

---
+++ tests/simplest.cc
@@ -0,0 +1,56 @@
+#include "catch.hpp"
+#include <fsm.hpp>
+
+/* Define an Event to trigger state machine transition */
+struct EventStep {};
+
+/* Starting state, StateA with ID 1 */
+struct StateA : public fsm::state<1>
+{
+	/* Each state has to have enter() and exit() methods
+	 * called when entering or leaving the state */
+	void enter() {}
+	void exit() {}
+
+	/* If there is transition from this state triggered by
+	 * some particular event you need to create event() method
+	 * with argument of Event type. Otherwise you will get
+	 * compile time error. */
+	bool event(const EventStep &)
+	{
+		/* return true to allow transition, false otherwise */
+		return true;
+	}
+};
+
+/* Last state, StateB with ID 2 */
+struct StateB : public fsm::state<2>
+{
+	/* as before, enter() and exit() methods but no event()
+	 * method because there won't be any outgoing transitions
+	 * from this state. */
+	void enter() {}
+	void exit() {}
+};
+
+/* Simple state transition table, each element of
+ * fsm::transitions describe one transition in form of:
+ * transition<StartState, Trigger, EndState>. */ 
+using transition_table = fsm::transitions<
+	/* in this case there is only one transition, from
+	 * StateA to StateB triggered by EventStep */
+	fsm::transition<StateA, EventStep, StateB>
+>;
+
+TEST_CASE("Two state one transition", "[fsm]")
+{
+	/* create instance of state machine with predefined
+	 * transition table */
+	fsm::fsm<transition_table> sm;
+
+	REQUIRE(sm.currentState() == 1);
+	sm.on(EventStep {});
+	REQUIRE(sm.currentState() == 2);
+	sm.on(EventStep {});
+	REQUIRE(sm.currentState() == 2);
+}
